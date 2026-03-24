0f15fb8c1883a8dcff431f9437d0c9b4306388ff
bruel.adrien <bruel.adrienpro@gmail.com>
2026-03-24 14:46:51 +0100

    Mise en place de la pipeline CI avec compilation, tests et génération du jar

--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
@@ -24,6 +24,7 @@ import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import gpsUtil.GpsUtil;
@@ -45,6 +46,7 @@ public class TourGuideService {
 	public final Tracker tracker;
 	boolean testMode = true;
 
+	@Autowired
 	public TourGuideService(GpsUtil gpsUtil, RewardsService rewardsService) {
 		this(gpsUtil, rewardsService, true);
 	}
